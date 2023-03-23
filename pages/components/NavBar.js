9e2a3b17ec78ae2b835e08d5d2fc4e8e546b797a
Monique Santos <monique@blackmagicbox.com.br>
2023-03-23 13:50:14 +0100

    Layout component created

---
+++ pages/components/NavBar.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NavBar() {
+  return (
+    <div>
+      <div>
+        <Image
+          src="/images/pokeball.png"
+          width="30"
+          height="30"
+          alt="PokeNext"
+        />
+        <h1>PokeNext</h1>
+      </div>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
